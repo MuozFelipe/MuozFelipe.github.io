9ea4ab04e17a047a7d29c325c07661e526821bd6
MuozFelipe <135499045+MuozFelipe@users.noreply.github.com>
2023-07-03 00:43:26 +1000

    prueba 4

---
+++ MiAplicacion/PHP - Copy/Controller/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+class Controller_Usuarionuevo {
+
+public function __construct($Nombre,$Email,$Password)
+{
+    return view ("view");
+}
+
+}
+
+
+
+require("/Users/polar/OneDrive/Desktop/MiAplicacion/PHP/View/View.php");
+?>
---
+++ MiAplicacion/PHP/Controller/Controler_Contacto.php
@@ -0,0 +1,15 @@
+<?php
+
+class Controller_Contactonuevo {
+
+public function __construct($Nombre2,$Mensaje)
+{
+    return view ("view");
+}
+
+}
+
+
+
+require("/Users/polar/OneDrive/Desktop/MiAplicacion/PHP/View/View_Contacto.php");
+?>
